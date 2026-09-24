9fa5d07e315a2e5abcd7e5e17fa13e1d28b36e3c
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-09-24 09:38:44 +0200

    Ajout de la classe BankAccount

--- src/fr/fms/entities/BankAccount.java
+++ src/fr/fms/entities/BankAccount.java
@@ -1,5 +1,101 @@
 package fr.fms.entities;
 
+/**
+ * Représente un compte bancaire.
+ * 
+ * Un compte possède un numéro de compte, un titulaire et un solde.
+ */
 public class BankAccount {
+    // =========================
+    // ATTRIBUTS
+    // =========================
+	
+	private String accountNumber;
+	private String holder;
+	private double balance;
+	
+    // =========================
+    // CONSTRUCTEUR
+    // =========================
+	
+	/** Crée un compte bancaire avec un numéro, un titulaire et un solde.
+	 * 
+	 * @param accountNumber numéro du compte bancaire
+	 * @param holder titulaire du compte bancaire
+	 * @param balance solde du compte bancaire
+	 */
+	public BankAccount(String accountNumber, String holder, double balance) {
+		this.accountNumber = accountNumber;
+		this.holder = holder;
+		this.balance = balance;
+	}
+	
+    // =========================
+    // ACCESSEURS
+    // =========================
+	
+	/** Récupère le numéro du compte bancaire
+	 * @return the accountNumber numéro du compte bancaire
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	/** Enregistre le numéro du compte bancaire
+	 * @param accountNumber numéro du compte bancaire
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	/** Récupère le titulaire du compte bancaire
+	 * @return the holder titulaire du compte bancaire
+	 */
+	public String getHolder() {
+		return holder;
+	}
+	/** Enregistre le titulaire du compte bancaire
+	 * @param holder titulaire du compte bancaire
+	 */
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+	/** Récupère le solde du compte bancaire
+	 * @return the balance solde du compte bancaire
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	/** Enregistre le solde du compte bancaire
+	 * @param balance solde du compte bancaire
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
+	// =========================
+    // MÉTHODES
+    // =========================
+	
+	/** Retourne les informations du compte bancaire sous forme de chaine.
+	 * @return représentation textuelle du compte bancaire
+	 */
+	@Override
+	public String toString() {
+		return "BankAccount [accountNumber=" + accountNumber + ", holder=" + holder + ", balance=" + balance + "]";
+	}
+	
+	// To do
+	public void deposit(double amount) {
+		
+	}
+	
+	// To do
+	public void withdraw(double amount) {
+		
+	}
+	
+	// To do
+	public void transfer(BankAccount destination, double amount) {
+		
+	}
+	
 }
